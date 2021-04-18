f27154475f8a5bf192c1e757164fe10d9c7f9a72
Andrew Yu <2521andy@gmail.com>
2021-04-18 06:11:26 -0700

    Co-authored-by: SleepyMinecrafter <SleepyMinecrafter@users.noreply.github.com> Co-authored-by: Shreya Nair <shreyanair612@users.noreply.github.com>

--- wyr.js
+++ wyr.js
@@ -7,4 +7,25 @@ $.getJSON("https://burritoapi.github.io/burrito-api/wyr.json").done(function( da
     document.getElementById('title_text2').innerHTML = data['choices'][choice].red;
     document.getElementById('background').src = data['choices'][choice]['blue_img'];    
     document.getElementById('background2').src = data['choices'][choice]['red_img'];
+    document.getElementById('background-back').src = data['choices'][choice]['blue_img'];
+    document.getElementById('background2-back').src = data['choices'][choice]['red_img'];
+    document.getElementById('percentage').innerHTML = data['choices'][choice]['blue_stat'];
+    document.getElementById('percentage2').innerHTML = data['choices'][choice]['red_stat'];
 });
+
+function getData(){
+    $.getJSON("https://burritoapi.github.io/burrito-api/wyr.json").done(function( data ) {
+    var choice = Math.floor(Math.random() * data['choices'].length)
+    
+    console.log("Blue: " + data['choices'][choice].blue + "\nBlue_Stats: " + data['choices'][choice].blue_stat + "\nRed: " + data['choices'][choice].red + "\nRed_Stats: " + data['choices'][choice].red_stat); 
+
+    document.getElementById('title_text').innerHTML = data['choices'][choice].blue;
+    document.getElementById('title_text2').innerHTML = data['choices'][choice].red;
+    document.getElementById('background').src = data['choices'][choice]['blue_img'];    
+    document.getElementById('background2').src = data['choices'][choice]['red_img'];
+    document.getElementById('background-back').src = data['choices'][choice]['blue_img'];
+    document.getElementById('background2-back').src = data['choices'][choice]['red_img'];
+    document.getElementById('percentage').innerHTML = data['choices'][choice]['blue_stat'];
+    document.getElementById('percentage2').innerHTML = data['choices'][choice]['red_stat'];
+    });
+}i
