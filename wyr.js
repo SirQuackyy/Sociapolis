deedcd04ade2b8274b26fa388d6167721219ed20
Andrew Yu <2521andy@gmail.com>
2021-04-18 00:56:16 -0700

    Co-authored-by: Shreya Nair <shreyanair612@users.noreply.github.com> Co-authored-by: SleepyMinecrafter <SleepyMinecrafter@users.noreply.github.com>

---
+++ wyr.js
@@ -0,0 +1,10 @@
+$.getJSON("https://burritoapi.github.io/burrito-api/wyr.json").done(function( data ) {
+    var choice = Math.floor(Math.random() * data['choices'].length)
+    
+    console.log("Blue: " + data['choices'][choice].blue + "\nBlue_Stats: " + data['choices'][choice].blue_stat + "\nRed: " + data['choices'][choice].red + "\nRed_Stats: " + data['choices'][choice].red_stat); 
+
+    document.getElementById('title_text').innerHTML = data['choices'][choice].blue;
+    document.getElementById('title_text2').innerHTML = data['choices'][choice].red;
+    document.getElementById('background').src = data['choices'][choice]['blue_img'];    
+    document.getElementById('background2').src = data['choices'][choice]['red_img'];
+});
